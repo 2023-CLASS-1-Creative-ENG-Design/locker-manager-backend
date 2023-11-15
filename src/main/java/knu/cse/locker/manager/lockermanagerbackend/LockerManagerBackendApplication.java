3486e9f7c9921b83e0f660f62407514d5fda33f4
lsmin3388 <lsmin3388_@naver.com>
2023-11-15 11:47:55 +0900

    Feat: 스프링부트 개발 환경 세팅

---
+++ src/main/java/knu/cse/locker/manager/lockermanagerbackend/LockerManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package knu.cse.locker.manager.lockermanagerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LockerManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LockerManagerBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/knu/cse/locker/manager/lockermanagerbackend/LockerManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package knu.cse.locker.manager.lockermanagerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LockerManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
