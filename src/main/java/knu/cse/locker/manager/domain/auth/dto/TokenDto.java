a254e9878ec5dc3f9880b72ec70782b485e4fa9e
lsmin3388 <lsmin3388_@naver.com>
2023-11-27 20:48:49 +0900

    Feat: 블록체인 API 연결 추가

--- src/main/java/knu/cse/locker/manager/domain/auth/dto/TokenDto.java
+++ src/main/java/knu/cse/locker/manager/domain/auth/dto/TokenDto.java
@@ -2,15 +2,18 @@ package knu.cse.locker.manager.domain.auth.dto;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
+import lombok.*;
 
 @Getter
-@Builder
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TokenDto {
     private String grantType;
     private String accessToken;
+
+    @Builder
+    public TokenDto(String grantType, String accessToken) {
+        this.grantType = grantType;
+        this.accessToken = accessToken;
+    }
 }
